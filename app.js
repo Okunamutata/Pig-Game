bb40f78b1e660cfdd9d680062c21c120d9ff6fc1
Okunamutata <tokunna@gmail.com>
2019-04-07 00:12:02 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,114 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, gamePlaying;
+
+
+init();
+gamePlaying = true;
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    if(gamePlaying){
+
+    
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    var diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round Score if the rolled number is not a 1
+    if(dice !== 1){
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+        //switch player
+        nextPlayer();
+        //document.querySelector('.dice').style.display = 'none';
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+
+    }
+
+    }
+    
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    //When hold is cliked, att score tp player global score
+    if(gamePlaying){
+    scores[activePlayer] += roundScore;
+
+    //Update the Ui score
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    //check if player won the game
+
+    if(scores[activePlayer] >= 20){
+        gamePlaying = false;
+        document.querySelector('#name-' + activePlayer).textContent = '!Winner!'
+        //document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    }
+    else{
+        nextPlayer();
+    }
+ } 
+});
+
+function nextPlayer(){
+    //switch player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+function init(){
+    scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+
+document.querySelector('.player-1-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+
+
+}
+//btn is a call back, we do not call it 
+//annymous function is a function without a name, it can not be reused
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
